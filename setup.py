3b7716c347a60bdc7493a8b2b9d366625d6912ee
Edgar Y. Walker <edgar.walker@gmail.com>
2018-04-03 15:11:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
     author_email='',
     url='https://github.com/eywalker/pixelcnn-pp',
     packages=find_packages(exclude=[]),
-    install_requires=['numpy', 'tqdm', 'attorch'],
+    install_requires=['numpy', 'tqdm', 'pytorch'],
 )
