6b28db96719307f44c14eeb3a71bf63adc4ff191
Edgar Y. Walker <edgar.walker@gmail.com>
2018-09-13 14:19:39 -0500

    Make installable

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='pixel_cnn',
+    version='0.0.0',
+    description='PixelCNN',
+    author='',
+    author_email='',
+    url='https://github.com/eywalker/pixelcnn-pp',
+    packages=find_packages(exclude=[]),
+    install_requires=['numpy', 'tqdm', 'attorch'],
+)
